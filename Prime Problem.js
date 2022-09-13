79dcfbd43b1fd6bfe7febed770ead3d778af9b22
Nikahatqureshi <qureshinikahat@gmail.com>
2022-09-13 21:07:55 +0530

    code logic

--- Prime Problem.js
+++ Prime Problem.js
@@ -1,1 +1,12 @@
 COPY PASTE KR DO
+
+count=0;
+     for(let i=1;i<=num;i++){
+         if(num%i===0){
+             count++;
+         }
+     }if (count==2){
+         console.log("Yes")
+     }else{
+         console.log("No")
+     }
